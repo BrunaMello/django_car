81dd72d5ae6a80407a4b87d414a543fd1dc5eed1
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-04-06 12:38:12 +0100

    037 - Configurando o Admin do nosso model (Car)

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from cars.models import Car
 
-# Register your models here. Tem relacao com o admin do django. Tela de administracao de conteudo. Viazualizacao permitidas somente aos admins do sistema.
+
+# Register your models here. Tem relacao com o admin do django. Tela de administracao de conteudo. Vizualizacao permitidas somente aos admins do sistema.
+
+#listar na view do admin do sistema
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    search_fields = ('model',)
+
+#registrar as tabelas e as configuracoes
+admin.site.register(Car, CarAdmin)
+
