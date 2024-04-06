0571ae00802037603ba81883af2dc6d9c54cfbad
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-04-06 13:03:48 +0100

    039 - Criando modelo e admin de marcas (ForeignKey)

--- cars/admin.py
+++ cars/admin.py
@@ -1,14 +1,26 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
 
 
 # Register your models here. Tem relacao com o admin do django. Tela de administracao de conteudo. Vizualizacao permitidas somente aos admins do sistema.
 
 #listar na view do admin do sistema
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
     search_fields = ('model', 'brand')
 
+
 #registrar as tabelas e as configuracoes
 admin.site.register(Car, CarAdmin)
+admin.site.register(Brand, BrandAdmin)
+
+
+
+
 
