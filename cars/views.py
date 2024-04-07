efbe6ab71c99133b40941a6c15583d999244bda3
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-04-07 09:35:53 +0100

    043 - Organizando nossa View de Carros

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here. Todas as views, tem a logica que precisa aplicar para a visualizacao
 
 
+def cars_view(request):
+    return HttpResponse("Hello, world. You're at the cars view.")
