6f9ba7efd4741a9cc0ac6b901a8f95be541ed934
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-04-07 10:06:10 +0100

    046 - Linguagem de Templates do Django

--- cars/views.py
+++ cars/views.py
@@ -7,5 +7,6 @@ from django.shortcuts import render
 def cars_view(request):
     return render(
         request=request,
-        template_name='cars.html'
+        template_name='cars.html',
+        context={'cars': {'model': 'Astra 2.0'}}
     )
