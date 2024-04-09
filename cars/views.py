21d6a4166da0f373b7477048e93117ecb0ce7754
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-04-09 18:34:45 +0100

    047 - Buscando carros no Banco de Dados com Django ORM

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from cars.models import Car
 
 # Create your views here. Todas as views, tem a logica que precisa aplicar para a visualizacao
 
 
 def cars_view(request):
+    # orm fazendo a requisicao no banco de dados igual o select
+    #buscando todos os carros disponiveis
+    cars = Car.objects.all()
+
     return render(
         request=request,
         template_name='cars.html',
-        context={'cars': {'model': 'Astra 2.0'}}
+        context={'cars': cars}
     )
