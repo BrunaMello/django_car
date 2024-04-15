de543eec4adfb0e93da0ccd4d99d132e0192579f
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-04-15 18:33:47 +0100

    065 - Reescrevendo cars_view (View Base)

--- cars/views.py
+++ cars/views.py
@@ -1,29 +1,31 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from cars.models import Car
+from django.views import View
+
 from cars.forms import CarModelForm
+from cars.models import Car
 
 
 # Create your views here. Todas as views, tem a logica que precisa aplicar para a visualizacao
 
+class CarsView(View):
 
-def cars_view(request):
-    # orm fazendo a requisicao no banco de dados igual o select
-    # buscando todos os carros disponiveis
-    cars = Car.objects.all().order_by('model')
+    def get(self, request):
+        # orm fazendo a requisicao no banco de dados igual o select
+        # buscando todos os carros disponiveis
+        cars = Car.objects.all().order_by('model')
 
-    # verificar se tem busca especifica
-    search = request.GET.get('search')
+        # verificar se tem busca especifica
+        search = request.GET.get('search')
 
-    # se sim entra no if
-    if search:
-        cars = Car.objects.filter(model__icontains=search).order_by('model')
+        # se sim entra no if
+        if search:
+            cars = Car.objects.filter(model__icontains=search).order_by('model')
 
-    return render(
-        request=request,
-        template_name='cars.html',
-        context={'cars': cars}
-    )
+        return render(
+            request=request,
+            template_name='cars.html',
+            context={'cars': cars}
+        )
 
 
 def new_car_view(request):
