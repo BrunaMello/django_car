561b2b74d91a6b3cd97c01334296609f2b2a93d9
Bruna Mello <38960731+BrunaMello@users.noreply.github.com>
2024-04-15 18:47:54 +0100

    066 - Reescrevendo new_car_view (View Base)

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path
 
 from accounts.views import register_view, login_view, logout_view
-from cars.views import new_car_view, CarsView
+from cars.views import CarsView, NewCarView
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
@@ -12,5 +12,5 @@ urlpatterns = [
                   path('login/', login_view, name='login'),
                   path('logout/', logout_view, name='logout'),
                   path('cars/', CarsView.as_view(), name='cars_list'),
-                  path('new_car/', new_car_view, name='new_car'),
+                  path('new_car/', NewCarView.as_view(), name='new_car'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
